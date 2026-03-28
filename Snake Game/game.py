c005886284117d372fb1295089e62302192cb000
Abir8050 <210509040+mim000007@users.noreply.github.com>
2026-03-28 21:33:46 +0600

    Update game.py

--- Snake Game/game.py
+++ Snake Game/game.py
@@ -38,3 +38,45 @@ while True:
     wn.update()
 
 wn.mainloop()
+
+//
+
+ # Check for a collision with the border
+        if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segments.clear()
+
+            score = 0
+            delay = 0.1
+
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 18, "normal"))
+
+        # Check for a collision with the food
+        if head.distance(food) < 20:
+            x = random.randint(-290, 290)
+            y = random.randint(-290, 290)
+            food.goto(x, y)
+
+            new_segment = turtle.Turtle()
+            new_segment.speed(0)
+            new_segment.shape("square")
+            new_segment.color("grey")
+            new_segment.penup()
+            segments.append(new_segment)
+
+            delay -= 0.001
+            score += 10
+
+            if score > high_score:
+                high_score = score
+
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 18, "normal"))
+
+
