2677e73b9cdf3b0119ec19256fc4728ffae9d4c8
Abir8050 <210509040+mim000007@users.noreply.github.com>
2026-03-28 22:00:48 +0600

    Update game.py

--- Snake Game/game.py
+++ Snake Game/game.py
@@ -125,3 +125,34 @@ def run_game():
                           cursor="hand2",
                           command=toggle_pause)
     pause_btn.place(relx=1.0, rely=0.0, anchor="ne", x=-8, y=8)
+
+    # Functions
+    def go_up():
+        if head.direction != "down":
+            head.direction = "up"
+
+    def go_down():
+        if head.direction != "up":
+            head.direction = "down"
+
+    def go_left():
+        if head.direction != "right":
+            head.direction = "left"
+
+    def go_right():
+        if head.direction != "left":
+            head.direction = "right"
+
+    def move():
+        if head.direction == "up":
+            y = head.ycor()
+            head.sety(y + 20)
+        if head.direction == "down":
+            y = head.ycor()
+            head.sety(y - 20)
+        if head.direction == "left":
+            x = head.xcor()
+            head.setx(x - 20)
+        if head.direction == "right":
+            x = head.xcor()
+            head.setx(x + 20)
